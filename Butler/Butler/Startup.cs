7eb5aa8c10873581807e571fcd8856c420b2c4e1
MatejH <matey574@gmail.com>
2017-04-22 15:14:44 +0200

    butler-config RE-INIT project

--- Butler/Butler/Startup.cs
+++ Butler/Butler/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.HttpOverrides;
 
-namespace Butler
+namespace butler
 {
     public class Startup
     {
