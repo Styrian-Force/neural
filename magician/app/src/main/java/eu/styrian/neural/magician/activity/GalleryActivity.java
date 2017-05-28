b4bfc7ef625290538d3d54f869321b312f9e40c4
Gal Kos <gal.kos@outlook.com>
2017-05-29 00:31:27 +0200

    magician MOVE api calls to ImageView Activity

--- magician/app/src/main/java/eu/styrian/neural/magician/activity/GalleryActivity.java
+++ magician/app/src/main/java/eu/styrian/neural/magician/activity/GalleryActivity.java
@@ -37,6 +37,7 @@ import eu.styrian.neural.magician.api.models.ImageTaskStatus;
 import eu.styrian.neural.magician.api.models.Value;
 import eu.styrian.neural.magician.api.request.ImageRequestFactory;
 import eu.styrian.neural.magician.api.utils.ApiServiceFactory;
+import eu.styrian.neural.magician.util.FileUtil;
 import eu.styrian.neural.magician.util.ImageProcessingUtil;
 import okhttp3.MultipartBody;
 import okhttp3.ResponseBody;
@@ -61,22 +62,11 @@ public class GalleryActivity extends AppCompatActivity {
     @BindView(R.id.button_send)
     public Button buttonSend;
 
-    private ImageService _imageService;
-    private ImageTaskService _imageTaskService;
-
-    private MagicianSpinnerDialog _dialog;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
         ButterKnife.bind(this);
-
-        this._imageService = ApiServiceFactory.getImageService();
-        this._imageTaskService = ApiServiceFactory.getImageTaskService();
-
-        FragmentManager fm = getSupportFragmentManager();
-        this._dialog = new MagicianSpinnerDialog();
     }
 
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -84,70 +74,11 @@ public class GalleryActivity extends AppCompatActivity {
             if (requestCode == SELECT_PICTURE) {
                 Uri selectedImageUri = data.getData();
 
-                selectedImagePath = getFilePath(this, selectedImageUri);
+                selectedImagePath = FileUtil.getFilePath(this, selectedImageUri);
 
                 imageView.setImageURI(selectedImageUri);
                 Log.d("", "ssss" + selectedImagePath);
             }
-        }
-    }
-
-    @TargetApi(Build.VERSION_CODES.KITKAT)
-    public static String getFilePath(Context context, Uri uri) {
-        int currentApiVersion;
-        try {
-            currentApiVersion = android.os.Build.VERSION.SDK_INT;
-        } catch (NumberFormatException e) {
-            //API 3 will crash if SDK_INT is called
-            currentApiVersion = 3;
-        }
-        if (currentApiVersion >= Build.VERSION_CODES.KITKAT) {
-            String filePath = "";
-            String wholeID = DocumentsContract.getDocumentId(uri);
-
-            // Split at colon, use second item in the array
-            String id = wholeID.split(":")[1];
-
-            String[] column = {MediaStore.Images.Media.DATA};
-
-            // where id is equal to
-            String sel = MediaStore.Images.Media._ID + "=?";
-
-            Cursor cursor = context.getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                    column, sel, new String[]{id}, null);
-
-            int columnIndex = cursor.getColumnIndex(column[0]);
-
-            if (cursor.moveToFirst()) {
-                filePath = cursor.getString(columnIndex);
-            }
-            cursor.close();
-            return filePath;
-        } else if (currentApiVersion <= Build.VERSION_CODES.HONEYCOMB_MR2 && currentApiVersion >= Build.VERSION_CODES.HONEYCOMB)
-
-        {
-            String[] proj = {MediaStore.Images.Media.DATA};
-            String result = null;
-            CursorLoader cursorLoader = new CursorLoader(
-                    context,
-                    uri, proj, null, null, null);
-            Cursor cursor = cursorLoader.loadInBackground();
-
-            if (cursor != null) {
-                int column_index =
-                        cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-                cursor.moveToFirst();
-                result = cursor.getString(column_index);
-            }
-            return result;
-        } else {
-
-            String[] proj = {MediaStore.Images.Media.DATA};
-            Cursor cursor = context.getContentResolver().query(uri, proj, null, null, null);
-            int column_index
-                    = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-            cursor.moveToFirst();
-            return cursor.getString(column_index);
         }
     }
 
@@ -162,176 +93,14 @@ public class GalleryActivity extends AppCompatActivity {
 
     @OnClick(R.id.button_send)
     public void sendImage(View view) {
-        _dialog.show(getSupportFragmentManager(), "some_tag");
-        Log.d("asd", "BUTTON SEND!" + this.selectedImagePath);
-
-        this.buttonSend.setEnabled(false);
-        if (this.selectedImagePath == null) {
-            Log.d("ERROR1", "ni datoteke!!! ne more poslati");
-            resetControls();
-            return;
-        }
-
-        File file = new File(this.selectedImagePath);
-
-        if (!file.exists()) {
-            Log.d("ERROR1", "ni datoteke!!! ne more poslati");
-            resetControls();
-            return;
-        }
-
-        Bitmap bitmap = getBitmap(file);
-        Bitmap resized = ImageProcessingUtil.resizeBitmap(bitmap, 500, 500);
-
-        if(resized == null) {
-            resetControls();
-
-        }
-
-        MultipartBody.Part imageFileBody = ImageRequestFactory.getInstance().generatePostRequest(file);
-
-        Observable<ImageTask> onImage = _imageService.post(imageFileBody);
-
-        onImage.subscribeOn(Schedulers.newThread())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Subscriber<ImageTask>() {
-                    @Override
-                    public final void onCompleted() {
-                    }
-
-                    @Override
-                    public final void onError(Throwable e) {
-                        Log.d("error", e.getMessage());
-                        resetControls();
-                    }
-
-                    @Override
-                    public final void onNext(ImageTask imageTask) {
-                        checkImageTaskStatus(imageTask);
-                    }
-                });
-    }
-
-
-    public void checkImageTaskStatus(final ImageTask imageTaskIn) {
-        if (imageTaskIn == null) {
-            Log.d(this.getClass().toString(), "ImageTask in null.");
-            resetControls();
-            return;
-        }
-
-        Observable<ImageTask> onImageTask = _imageTaskService.getById(imageTaskIn.getJobId());
-
-        onImageTask.delay(1, TimeUnit.SECONDS)
-                .subscribeOn(Schedulers.newThread())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Subscriber<ImageTask>() {
-                    @Override
-                    public final void onCompleted() {
-                    }
-
-                    @Override
-                    public final void onError(Throwable e) {
-                        Log.d("error", e.getMessage());
-                        resetControls();
-                    }
-
-                    @Override
-                    public final void onNext(ImageTask imageTask) {
-                        Log.d(this.getClass().toString(), "imageTask: " + imageTask.getJobId());
-
-                        if (imageTask == null) {
-                            Log.d(this.getClass().toString(), "ImageTask in null.");
-                            resetControls();
-                            return;
-                        }
-
-                        ImageTaskStatus status = ImageTaskStatus.parse(imageTask.getStatus());
-
-                        if (status == ImageTaskStatus.FINISHED) {
-                            getImage(imageTask);
-                            return;
-                        } else if (status == ImageTaskStatus.ERROR) {
-                            Log.d(this.getClass().toString(), "Error in image task.");
-                            resetControls();
-                            return;
-                        } else {
-                            Log.d(this.getClass().toString(), "CurrentStatus: " + imageTask.getStatus());
-                            checkImageTaskStatus(imageTask);
-                            return;
-                        }
-
-                    }
-                });
-    }
-
-    private void getImage(ImageTask imageTask) {
-        Log.d(this.getClass().toString(), "GET_IMAGE: " + imageTask.getJobId() + " " + imageTask.getStatus());
-
-        Observable<Response<ResponseBody>> onImage = _imageService.getById(imageTask.getJobId());
-        Log.i("", "neki - ");
-
-        onImage.subscribeOn(Schedulers.newThread())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Subscriber<Response<ResponseBody>>() {
-                    @Override
-                    public final void onCompleted() {
-                    }
-
-                    @Override
-                    public final void onError(Throwable e) {
-                        Log.d("error", e.getMessage());
-                        resetControls();
-                    }
-
-                    @Override
-                    public final void onNext(Response<ResponseBody> response) {
-                        Log.d("success", response.body() + " asd");
-                        Log.i("", "neki - success" + response.code());
-                        if (response.isSuccessful()) {
-                            Log.i("", "neki - success" + response.body().contentLength());
-                            Bitmap bitmap = BitmapFactory.decodeStream(response.body().byteStream());
-                            imageView.setImageBitmap(bitmap);
-                            goToImageView(bitmap);
-                        }
-                    }
-                });
+        Intent intent = new Intent(getApplicationContext(), ImageViewActivity.class);
+        intent.putExtra("imagePath", this.selectedImagePath);
+        startActivity(intent);
     }
 
     private void resetControls() {
-        this._dialog.dismiss();
         this.buttonSend.setEnabled(true);
     }
 
-    private Bitmap getBitmap(File file) {
-        FileInputStream fis = null;
-        try {
-            fis = new FileInputStream(file);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            return null;
-        }
 
-        Bitmap bitmap = BitmapFactory.decodeStream(fis);
-        return bitmap;
-    }
-
-    private void goToImageView(final Bitmap bitmap) {
-        runOnUiThread(new Runnable() {
-
-            @Override
-            public void run() {
-                Log.d(GalleryActivity.class.toString(), "GOTO Image View");
-                Intent intent = new Intent(getApplicationContext(), ImageViewActivity.class);
-                //ByteArrayOutputStream bs = new ByteArrayOutputStream();
-                //bitmap.compress(Bitmap.CompressFormat.PNG, 50, bs);
-                //intent.putExtra("byteArray", bs.toByteArray());
-                //intent.putExtra("bitmap", bitmap);
-                intent.putExtra("neki", "neki");
-                resetControls();
-                startActivity(intent);
-            }
-        });
-
-    }
 }
--- magician/app/src/main/java/eu/styrian/neural/magician/util/FileUtil.java
+++ magician/app/src/main/java/eu/styrian/neural/magician/util/FileUtil.java
@@ -1,8 +1,17 @@
 package eu.styrian.neural.magician.util;
 
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.CursorLoader;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -13,12 +22,12 @@ import java.util.Date;
 
 public class FileUtil {
 
-    public static File createTemporaryFile (String fileName, String extensionName, Context context) {
+    public static File createTemporaryFile(String fileName, String extensionName, Context context) {
         File outputDir = context.getCacheDir(); // context being the Activity pointer
         File outputFile = null;
         try {
-            String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
-            outputFile = File.createTempFile("picture" + timeStamp, "jpg", outputDir);
+            String timeStamp = new SimpleDateFormat("_yyyy.MM.dd.HH.mm.ss").format(new Date());
+            outputFile = File.createTempFile(fileName + timeStamp, extensionName, outputDir);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -26,4 +35,81 @@ public class FileUtil {
         return outputFile;
     }
 
+    public static void saveBitmapToFile(File file, Bitmap bitmap) {
+        FileOutputStream out = null;
+        try {
+            out = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, out); // bmp is your Bitmap instance
+            // PNG is a lossless format, the compression factor (100) is ignored
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (out != null) {
+                    out.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    public static String getFilePath(Context context, Uri uri) {
+        int currentApiVersion;
+        try {
+            currentApiVersion = android.os.Build.VERSION.SDK_INT;
+        } catch (NumberFormatException e) {
+            //API 3 will crash if SDK_INT is called
+            currentApiVersion = 3;
+        }
+        if (currentApiVersion >= Build.VERSION_CODES.KITKAT) {
+            String filePath = "";
+            String wholeID = DocumentsContract.getDocumentId(uri);
+
+            // Split at colon, use second item in the array
+            String id = wholeID.split(":")[1];
+
+            String[] column = {MediaStore.Images.Media.DATA};
+
+            // where id is equal to
+            String sel = MediaStore.Images.Media._ID + "=?";
+
+            Cursor cursor = context.getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    column, sel, new String[]{id}, null);
+
+            int columnIndex = cursor.getColumnIndex(column[0]);
+
+            if (cursor.moveToFirst()) {
+                filePath = cursor.getString(columnIndex);
+            }
+            cursor.close();
+            return filePath;
+        } else if (currentApiVersion <= Build.VERSION_CODES.HONEYCOMB_MR2 && currentApiVersion >= Build.VERSION_CODES.HONEYCOMB)
+
+        {
+            String[] proj = {MediaStore.Images.Media.DATA};
+            String result = null;
+            CursorLoader cursorLoader = new CursorLoader(
+                    context,
+                    uri, proj, null, null, null);
+            Cursor cursor = cursorLoader.loadInBackground();
+
+            if (cursor != null) {
+                int column_index =
+                        cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                cursor.moveToFirst();
+                result = cursor.getString(column_index);
+            }
+            return result;
+        } else {
+
+            String[] proj = {MediaStore.Images.Media.DATA};
+            Cursor cursor = context.getContentResolver().query(uri, proj, null, null, null);
+            int column_index
+                    = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        }
+    }
 }
