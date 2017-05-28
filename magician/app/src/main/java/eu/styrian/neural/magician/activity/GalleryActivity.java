2b34cea4ca5c59e5ccb9bb89e00bd597bb71bfe3
Gal Kos <gal.kos@outlook.com>
2017-05-28 22:23:27 +0200

    magician ADD Spinner

--- magician/app/src/main/java/eu/styrian/neural/magician/activity/GalleryActivity.java
+++ magician/app/src/main/java/eu/styrian/neural/magician/activity/GalleryActivity.java
@@ -11,6 +11,7 @@ import android.net.Uri;
 import android.os.Build;
 import android.provider.DocumentsContract;
 import android.provider.MediaStore;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -25,6 +26,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import eu.styrian.neural.magician.R;
+import eu.styrian.neural.magician.activity.fragment.MagicianSpinnerDialog;
 import eu.styrian.neural.magician.api.interfaces.ImageService;
 import eu.styrian.neural.magician.api.interfaces.ImageTaskService;
 import eu.styrian.neural.magician.api.models.ImageTask;
@@ -58,6 +60,8 @@ public class GalleryActivity extends AppCompatActivity {
     private ImageService _imageService;
     private ImageTaskService _imageTaskService;
 
+    private MagicianSpinnerDialog _dialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -66,6 +70,9 @@ public class GalleryActivity extends AppCompatActivity {
 
         this._imageService = ApiServiceFactory.getImageService();
         this._imageTaskService = ApiServiceFactory.getImageTaskService();
+
+        FragmentManager fm = getSupportFragmentManager();
+        this._dialog = new MagicianSpinnerDialog();
     }
 
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -82,20 +89,15 @@ public class GalleryActivity extends AppCompatActivity {
     }
 
     @TargetApi(Build.VERSION_CODES.KITKAT)
-    public static String getFilePath(Context context, Uri uri)
-    {
+    public static String getFilePath(Context context, Uri uri) {
         int currentApiVersion;
-        try
-        {
+        try {
             currentApiVersion = android.os.Build.VERSION.SDK_INT;
-        }
-        catch(NumberFormatException e)
-        {
+        } catch (NumberFormatException e) {
             //API 3 will crash if SDK_INT is called
             currentApiVersion = 3;
         }
-        if (currentApiVersion >= Build.VERSION_CODES.KITKAT)
-        {
+        if (currentApiVersion >= Build.VERSION_CODES.KITKAT) {
             String filePath = "";
             String wholeID = DocumentsContract.getDocumentId(uri);
 
@@ -112,14 +114,12 @@ public class GalleryActivity extends AppCompatActivity {
 
             int columnIndex = cursor.getColumnIndex(column[0]);
 
-            if (cursor.moveToFirst())
-            {
+            if (cursor.moveToFirst()) {
                 filePath = cursor.getString(columnIndex);
             }
             cursor.close();
             return filePath;
-        }
-        else if (currentApiVersion <= Build.VERSION_CODES.HONEYCOMB_MR2 && currentApiVersion >= Build.VERSION_CODES.HONEYCOMB)
+        } else if (currentApiVersion <= Build.VERSION_CODES.HONEYCOMB_MR2 && currentApiVersion >= Build.VERSION_CODES.HONEYCOMB)
 
         {
             String[] proj = {MediaStore.Images.Media.DATA};
@@ -129,17 +129,14 @@ public class GalleryActivity extends AppCompatActivity {
                     uri, proj, null, null, null);
             Cursor cursor = cursorLoader.loadInBackground();
 
-            if (cursor != null)
-            {
+            if (cursor != null) {
                 int column_index =
                         cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
                 cursor.moveToFirst();
                 result = cursor.getString(column_index);
             }
             return result;
-        }
-        else
-        {
+        } else {
 
             String[] proj = {MediaStore.Images.Media.DATA};
             Cursor cursor = context.getContentResolver().query(uri, proj, null, null, null);
@@ -151,7 +148,7 @@ public class GalleryActivity extends AppCompatActivity {
     }
 
     public String getPath1(Uri uri) {
-        String[] projection = { MediaStore.Images.Media.DATA };
+        String[] projection = {MediaStore.Images.Media.DATA};
         Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
         int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
         cursor.moveToFirst();
@@ -172,73 +169,25 @@ public class GalleryActivity extends AppCompatActivity {
 
     @OnClick(R.id.button_send)
     public void sendImage(View view) {
+        _dialog.show(getSupportFragmentManager(), "some_tag");
         Log.d("asd", "BUTTON SEND!" + this.selectedImagePath);
 
         this.buttonSend.setEnabled(false);
-        if(this.selectedImagePath == null) {
+        if (this.selectedImagePath == null) {
             Log.d("ERROR1", "ni datoteke!!! ne more poslati");
-            this.buttonSend.setEnabled(true);
+            resetControls();
             return;
         }
 
         File file = new File(this.selectedImagePath);
 
-        if(!file.exists()) {
+        if (!file.exists()) {
             Log.d("ERROR1", "ni datoteke!!! ne more poslati");
-            this.buttonSend.setEnabled(true);
+            resetControls();
             return;
         }
 
         MultipartBody.Part imageFileBody = ImageRequestFactory.getInstance().generatePostRequest(file);
-
-        /* Call<ResponseBody> call = imageService.post(imageFileBody);
-
-        call.enqueue(new Callback<ResponseBody>() {
-            @Override
-            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-
-                Log.i("", "neki - success" + response.code());
-                if (response.isSuccessful()) {
-                    Log.i("", "neki - success" + response.body().contentLength());
-                    Bitmap bitmap = BitmapFactory.decodeStream(response.body().byteStream());
-                    imageView.setImageBitmap(bitmap);
-                }
-                buttonSend.setEnabled(true);
-            }
-
-            @Override
-            public void onFailure(Call<ResponseBody> call, Throwable t) {
-                Log.e("", " - fail" + t.getMessage());
-            }
-        });
-
-        Observable<Response<ResponseBody>> onImage = imageService.getById("2017-05-15_16-37-09.770");
-        Log.i("", "neki - ");
-        onImage.subscribeOn(Schedulers.newThread())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Subscriber<Response<ResponseBody>>() {
-                    @Override
-                    public final void onCompleted() {
-                    }
-
-                    @Override
-                    public final void onError(Throwable e) {
-                        Log.d("error", e.getMessage());
-                    }
-
-                    @Override
-                    public final void onNext(Response<ResponseBody> response) {
-                        Log.d("success", response.body() + " asd");
-                        Log.i("", "neki - success" + response.code());
-                        if (response.isSuccessful()) {
-                            Log.i("", "neki - success" + response.body().contentLength());
-                            Bitmap bitmap = BitmapFactory.decodeStream(response.body().byteStream());
-                            imageView.setImageBitmap(bitmap);
-                        }
-                        buttonSend.setEnabled(true);
-                    }
-                });
-               */
 
         Observable<ImageTask> onImage = _imageService.post(imageFileBody);
         Log.i("", "neki - ");
@@ -252,6 +201,7 @@ public class GalleryActivity extends AppCompatActivity {
                     @Override
                     public final void onError(Throwable e) {
                         Log.d("error", e.getMessage());
+                        resetControls();
                     }
 
                     @Override
@@ -263,11 +213,12 @@ public class GalleryActivity extends AppCompatActivity {
 
 
     public void checkImageTaskStatus(final ImageTask imageTaskIn) {
-        if(imageTaskIn == null) {
+        if (imageTaskIn == null) {
             Log.d(this.getClass().toString(), "ImageTask in null.");
+            resetControls();
             return;
         }
-        
+
         Observable<ImageTask> onImageTask = _imageTaskService.getById(imageTaskIn.getJobId());
 
         onImageTask.delay(1, TimeUnit.SECONDS)
@@ -281,28 +232,30 @@ public class GalleryActivity extends AppCompatActivity {
                     @Override
                     public final void onError(Throwable e) {
                         Log.d("error", e.getMessage());
+                        resetControls();
                     }
 
                     @Override
                     public final void onNext(ImageTask imageTask) {
                         Log.d(this.getClass().toString(), "imageTask: " + imageTask.getJobId());
 
-                        if(imageTask == null) {
+                        if (imageTask == null) {
                             Log.d(this.getClass().toString(), "ImageTask in null.");
+                            resetControls();
                             return;
                         }
 
                         ImageTaskStatus status = ImageTaskStatus.parse(imageTask.getStatus());
 
-                        if(status == ImageTaskStatus.FINISHED) {
+                        if (status == ImageTaskStatus.FINISHED) {
                             getImage(imageTask);
+                            resetControls();
                             return;
-                        }
-                        else if (status == ImageTaskStatus.ERROR) {
+                        } else if (status == ImageTaskStatus.ERROR) {
                             Log.d(this.getClass().toString(), "Error in image task.");
+                            resetControls();
                             return;
-                        }
-                        else  {
+                        } else {
                             Log.d(this.getClass().toString(), "CurrentStatus: " + imageTask.getStatus());
                             checkImageTaskStatus(imageTask);
                             return;
@@ -317,6 +270,7 @@ public class GalleryActivity extends AppCompatActivity {
 
         Observable<Response<ResponseBody>> onImage = _imageService.getById(imageTask.getJobId());
         Log.i("", "neki - ");
+
         onImage.subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(new Subscriber<Response<ResponseBody>>() {
@@ -327,6 +281,7 @@ public class GalleryActivity extends AppCompatActivity {
                     @Override
                     public final void onError(Throwable e) {
                         Log.d("error", e.getMessage());
+                        resetControls();
                     }
 
                     @Override
@@ -338,9 +293,13 @@ public class GalleryActivity extends AppCompatActivity {
                             Bitmap bitmap = BitmapFactory.decodeStream(response.body().byteStream());
                             imageView.setImageBitmap(bitmap);
                         }
-                        buttonSend.setEnabled(true);
+                        resetControls();
                     }
                 });
     }
 
+    private void resetControls() {
+        this._dialog.dismiss();
+        this.buttonSend.setEnabled(true);
+    }
 }
